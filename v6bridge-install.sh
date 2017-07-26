5316e53660d18bf8098d738224232e42c0f45bea
Paul Michali <pc@michali.net>
2017-07-26 14:31:59 -0400

    Parameterize the config file and use that during the install so that each node gets a unique CIDR/GW.

--- v6bridge-install.sh
+++ v6bridge-install.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+# Setup Subnets
+INSTANCE=`hostname | cut -d"-" -f 3`
+CNI_IPV6_DEFAULT_CIDR="2001:${INSTANCE}::/64"
+CNI_IPV6_DEFAULT_GW="2001::${INSTANCE}::1"
+CNI_IPV4_DEFAULT_CIDR="10.244.${INSTANCE}.0/24"
+CNI_IPV4_DEFAULT_GW="10.244.${INSTANCE}.1"
+CNI_IPV6_CIDR="${CNI_IPV6_CIDR:-$CNI_IPV6_DEFAULT_CIDR}"
+CNI_IPV6_GW="${CNI_IPV6_GW:-$CNI_IPV6_DEFAULT_GW}"
+CNI_IPV4_CIDR="${CNI_IPV4_CIDR:-$CNI_IPV4_DEFAULT_CIDR}"
+CNI_IPV4_GW="${CNI_IPV4_GW:-$CNI_IPV4_DEFAULT_GW}"
+
 # Install binaries
-set -x
+set -xeu
+set -o pipefail
+
 curl -sSL --retry 5 https://github.com/containernetworking/plugins/releases/download/v0.6.0-rc1/cni-plugins-amd64-v0.6.0-rc1.tgz | \
     tar -C /opt/cni/bin -xz
 
@@ -10,6 +23,13 @@ curl --retry 5 https://raw.githubusercontent.com/pmichali/k8s-tools/master/10-br
 
 # Enable IPv6
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
+
+# Modify config file
+sed -i'' "s@%subnet_1%@${CNI_IPV4_CIDR}@g" "/etc/cni/net.d/10-bridge.conf"
+sed -i'' "s@%gateway_1%@${CNI_IPV4_GW}@g" "/etc/cni/net.d/10-bridge.conf"
+sed -i'' "s@%subnet_2%@${CNI_IPV6_CIDR}@g" "/etc/cni/net.d/10-bridge.conf"
+sed -i'' "s@%gateway_2%@${CNI_IPV6_GW}@g" "/etc/cni/net.d/10-bridge.conf"
+
 
 set +x
 
